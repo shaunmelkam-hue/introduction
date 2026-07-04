d8aeefbec105931ba61c6ba1c63e528e3a6bfc5b
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-04 17:36:34 -0500

    Add files via upload

--- datetype_activtiy.py
+++ datetype_activtiy.py
@@ -2,9 +2,9 @@ number = 20
 print(number)
 print(type(number))
 
-number = 20.5
-print(number)
-print(type(number))
+number1 = 20.5
+print(number1)
+print(type(number1))
 
 result = True
 print(result)
@@ -13,3 +13,6 @@ print(type(result))
 name = "John"
 print(name)
 print(type(name))
+
+print(float(number))
+print(int(number1))
