069652d9cdd5eb2bf4267edfa6c52d8821d9a390
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-04 17:25:09 -0500

    Add files via upload

---
+++ datetype_activtiy.py
@@ -0,0 +1,15 @@
+number = 20
+print(number)
+print(type(number))
+
+number = 20.5
+print(number)
+print(type(number))
+
+result = True
+print(result)
+print(type(result))
+
+name = "John"
+print(name)
+print(type(name))
