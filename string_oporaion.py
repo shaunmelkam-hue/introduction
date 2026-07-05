6788d2648c3c529233ff4ab180733b6a3856811c
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-05 17:26:11 -0500

    Add files via upload

---
+++ string_oporaion.py
@@ -0,0 +1,17 @@
+name = "shaun"
+
+#length of string
+print(len(name))
+
+#indexing or positioning
+print(name[0])
+print(name[2])
+
+#slicing
+print(name[0:3])
+
+lname = "melkam"
+#concatenated or joining
+print(name + lname)
+
+print(name.upper())
