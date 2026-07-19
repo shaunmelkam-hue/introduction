0774f163ca2d1531a03ea5bf42513259521819d0
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-19 17:50:36 -0500

    Add files via upload

---
+++ andor_activity.py
@@ -0,0 +1,24 @@
+a = 8
+b = 4
+c = 0
+
+if a and b and c:
+    print("all the numbers have boolean value True.")
+else:
+    print("At least one of the numbers has boolean value False.")
+
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the numbers is greater than zero.")
+else:
+    print("no number is greater than zero.")
+
+if b > 0 or c > 0:
+    print("Either of the numbers is greater than zero.")
+else:
+    print("no number is greater than zero.")
